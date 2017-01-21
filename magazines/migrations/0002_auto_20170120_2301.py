b2b5230e596d72242539da7a3bdc0ae0f1917247
Frank Mullen <frankmullen@gmail.com>
2017-01-21 12:58:35 +0000

    all files to date

---
+++ magazines/migrations/0002_auto_20170120_2301.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('magazines', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='purchase',
+            name='subscription_end',
+            field=models.DateTimeField(default=datetime.datetime(2017, 1, 20, 23, 1, 48, 899253, tzinfo=utc)),
+        ),
+    ]
